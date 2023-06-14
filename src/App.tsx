188c8f28d83260fe492302e52599f8a04d248d39
iower <iower@tutanota.com>
2023-06-14 11:49:07 +0500

    Home: more styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,20 @@ function App() {
         (header)
       </header>
       <div className="panel p-4 pb-6 rounded-3xl bg-white">
-        🎙 <strong>Миша Двойняков</strong> заплатил 6 000 и <strong>Антон Костин</strong> заплатил 4 000, <strong>Настя</strong> должна 2 500, <strong>Маша</strong> должна 3 000, <strong>Ришат</strong> 1 000 <strong>Даша</strong>, все остальное
+        <div className="text-[12px] leading-[1.33em] font-medium">Сообщение</div>
+        <div className="mt-1">
+          🎙 <strong>Миша Двойняков</strong> заплатил 6 000 и <strong>Антон Костин</strong> заплатил 4 000, <strong>Настя</strong> должна 2 500, <strong>Маша</strong> должна 3 000, <strong>Ришат</strong> 1 000 <strong>Даша</strong>, все остальное
+        </div>
       </div>
 
       <div className="panel p-4 pb-6 rounded-3xl bg-white">
         <h2>Соотнесите людей</h2>
-        <div>...</div>
-        <button className="text-[#4094F7]" onClick={() => setOpen(true)}>
+        <div className="mt-1 text-[14px] leading-[20px]">Со временем мы запомним соотношения</div>
+        <div className="mt-4 overflow-y-auto">
+
+        </div>
+        <button className="text-[#4094F7] h-8 w-full text-left" onClick={() => setOpen(true)}>
+          <span className="h-6 w-6"></span>
           <span>Добавить ещё</span>
         </button>
       </div>
