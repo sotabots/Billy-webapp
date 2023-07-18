43d24788e6fa58e35c2e84c28a48dafdc1799a17
iower <iower@tutanota.com>
2023-07-18 16:36:21 +0500

    More debug

--- src/kit/Debug.tsx
+++ src/kit/Debug.tsx
@@ -1,10 +1,20 @@
 import Panel from './Panel'
 
 function Debug() {
+  const isEnabled = true
+
+  if (!isEnabled) {
+    return null
+  }
+
   return (
-    <Panel className="mt-10 text-[12px]">
+    <Panel className="mt-10 text-[12px] break-words">
       <h2>Debug</h2>
-      {location.href}
+      <strong>href = </strong>{location.href}
+      <br />
+      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+      {/* @ts-ignore */}
+      <strong>window.Telegram.Webapp = </strong>{JSON.stringify(window.Telegram?.WebApp)}
     </Panel>
   )
 }
--- src/kit/Screen.tsx
+++ src/kit/Screen.tsx
@@ -13,9 +13,9 @@ function Screen({ children, className }: TScreen) {
     <div className={cx('fixed top-0 left-0 w-full h-full overflow-y-auto text-text bg-bg2', className)}>
       <div className="limiter">
         {children}
-      </div>
 
-      <Debug />
+        <Debug />
+      </div>
     </div>
   )
 }
