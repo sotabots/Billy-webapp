ce729cde28db7aa60d4db65a50cc452286f28139
iower <iower@tutanota.com>
2023-07-25 17:17:30 +0500

    Fix mock

--- src/store/mock.ts
+++ src/store/mock.ts
@@ -1,4 +1,4 @@
-import { TCurrency, TUser } from '../types'
+import { TCurrency, TTransaction, TUser } from '../types'
 
 const transliterate = (word: string) => {
   const a = {"Ё":"YO","Й":"I","Ц":"TS","У":"U","К":"K","Е":"E","Н":"N","Г":"G","Ш":"SH","Щ":"SCH","З":"Z","Х":"H","Ъ":"","ё":"yo","й":"i","ц":"ts","у":"u","к":"k","е":"e","н":"n","г":"g","ш":"sh","щ":"sch","з":"z","х":"h","ъ":"","Ф":"F","Ы":"I","В":"V","А":"A","П":"P","Р":"R","О":"O","Л":"L","Д":"D","Ж":"ZH","Э":"E","ф":"f","ы":"i","в":"v","а":"a","п":"p","р":"r","о":"o","л":"l","д":"d","ж":"zh","э":"e","Я":"Ya","Ч":"CH","С":"S","М":"M","И":"I","Т":"T","Ь":"","Б":"B","Ю":"YU","я":"ya","ч":"ch","с":"s","м":"m","и":"i","т":"t","ь":"","б":"b","ю":"yu"}
@@ -38,17 +38,9 @@ const generateNames = (n: number) => {
 const _names = generateNames(6)
 const mockUsers = _names.map(_name => generateUser(_name))
 
-const generateUserRelations = (n: number) => {
-  return mockUsers.slice(0, n).map(user => ({
-    title: user._name,
-    user: Math.random() > 0.5 ? user : undefined,
-  }))
-}
-
-const mockUserRelations = generateUserRelations(4)
-
-const mockTransaction = mockUserRelations.map((relation, i) => ({
-  ...relation.user,
+const mockTransaction: TTransaction = mockUsers.slice(0, 4).map((user, i) => ({
+  spokenName: user._name,
+  user: Math.random() > 0.5 ? user : undefined,
   isPayed: i < 2,
   amount: Math.round(Math.random() * 1e2) // * 1e6) / 100 // todo
 }))
