3448e7382c772ee25a689356b7df67f01d0d45e9
iower <iower@tutanota.com>
2023-08-01 20:48:26 +0500

    Fix onclick jump

--- src/kit/Button.tsx
+++ src/kit/Button.tsx
@@ -12,7 +12,7 @@ function Button({ children, theme = 'default', disabled, onClick }: TButton) {
   const className = {
     'default': 'mx-auto w-full max-w-[500px] md:max-w-[300px] block h-10 bg-button text-buttonText rounded-md text-[14px] leading-[20px] font-semibold enabled:hover:brightness-110 enabled:active:brightness-[1.2] disabled:opacity-40 transition-all',
 
-    'text': 'h-8 text-[14px] leading-[24px] text-button hover:brightness-[1.2] active:brightness-[1.4] transition-all'
+    'text': 'h-6 text-[14px] leading-[24px] text-button hover:brightness-[1.2] active:brightness-[1.4] transition-all'
   }[theme]
 
   return (
