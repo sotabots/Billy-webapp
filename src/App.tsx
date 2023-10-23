c16a29a9c07d9598920fc50841e73de764c358b5
iower <iower@tutanota.com>
2023-10-23 11:58:40 +0500

    Demo mode

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 // import viteLogo from '/vite.svg'
 
 import { useTheme } from './hooks'
+import { useStore } from './store'
 
 import Check from './pages/Check'
 import SelectUser from './pages/SelectUser'
@@ -19,6 +20,7 @@ const queryClient = new QueryClient()
 
 function App() {
   const { isDarkTheme } = useTheme()
+  const { txId } = useStore()
 
   const router = createHashRouter([
     {
@@ -61,6 +63,9 @@ function App() {
       <div className={cx(isDarkTheme ? 'theme-dark' : 'theme-light')}>
         <RouterProvider router={router} />
         <SplashScreen />
+        {txId === null && (
+          <div className="fixed bottom-0 width-auto left-[50%] -translate-x-[50%] text-[13px] font-semibold px-2 py-1 bg-[#3a3] text-white rounded-t-md">Демо-режим</div>
+        )}
       </div>
     </QueryClientProvider>
   )
