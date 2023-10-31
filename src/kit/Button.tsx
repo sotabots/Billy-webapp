bf8417822672a42daff95a6a0800f14a31b18d71
iower <iower@tutanota.com>
2023-10-31 12:08:28 +0500

    Change Button styles

--- src/kit/Button.tsx
+++ src/kit/Button.tsx
@@ -14,19 +14,19 @@ type TButton = {
 
 function Button({ children, theme = 'default', isBottom, disabled, isBusy, onClick }: TButton) {
   const themeStyle = {
-    'default': 'mx-auto w-full max-w-[500px] md:max-w-[300px] block h-10 bg-button text-buttonText rounded-md text-[14px] leading-[20px] font-semibold enabled:hover:brightness-110 enabled:active:brightness-[1.2] disabled:opacity-40 transition-all',
+    'default': 'mx-auto w-full max-w-[600px] block h-10 bg-button text-buttonText rounded-md text-[14px] leading-[20px] font-semibold enabled:hover:brightness-110 enabled:active:brightness-[1.2] disabled:opacity-40 transition-all',
 
     'text': 'h-6 text-[14px] leading-[24px] text-button hover:brightness-[1.2] active:brightness-[1.4] transition-all'
   }[theme]
 
   return (
     <div className={cx(isBottom && 'h-10')}>{/* spacer */}
-      <div className={cx(isBottom ? 'fixed bottom-0 left-0 w-full px-4 pt-1 pb-2 bg-bg' : 'relative')}>{/* loader wrapper */}
+      <div className={cx(isBottom ? 'fixed bottom-0 left-0 w-full pt-1 bg-bg' : 'relative')}>{/* loader wrapper */}
         {isBottom && (
           <div className="absolute bottom-full left-0 w-full h-2 bg-gradient-to-t from-bg" />
         )}
         <button
-          className={cx(themeStyle)}
+          className={cx(themeStyle, isBottom && '!h-[56px]')}
           disabled={disabled || isBusy}
           onClick={onClick}
         >
