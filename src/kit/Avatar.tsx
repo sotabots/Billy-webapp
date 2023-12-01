1cbd407a2839ac7386d5e7862510fe8b2d0c1d48
iower <iower@tutanota.com>
2023-12-01 13:55:14 +0500

    Fix Avatar

--- src/kit/Avatar.tsx
+++ src/kit/Avatar.tsx
@@ -1,5 +1,8 @@
 import { useTheme } from '../hooks'
+import { useStore } from '../store'
 import type { TUser } from '../types'
+
+const apiUrl = import.meta.env.VITE_API_URL
 
 type TAvatar = {
   user?: TUser,
@@ -23,8 +26,12 @@ function Avatar({ user, size = 40 }: TAvatar) {
   const color = !user ? placeholderColor: (['#e17076', '#faa774', '#a695e7', '#7bc862', '#6ec9cb', '#65aadd', '#ee7aae'])[Math.abs(Number(user._id)) % 7 || 0] // peerColor
   const backgroundColor = color + '44'
 
+  const { txId } = useStore()
   const url = user?.profile_photo
-  const backgroundImage = url ? `url(${url})` : undefined
+  const backgroundImage = !url
+    ? undefined
+    : `url(${txId ? `${apiUrl}${url}` : url})`
+
   const letters = (!url && fullName) ? getLetters(fullName) : null
 
   return (
