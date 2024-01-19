420815bc22f3769ac39399840e18ec335cb0bcdf
iower <iower@tutanota.com>
2024-01-19 23:46:49 +0500

    Simplify i18n fs

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,8 +2,8 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import translationEn from './en/translation.json';
-import translationRu from './ru/translation.json';
+import translationEn from './translation-en.json';
+import translationRu from './translation-ru.json';
 
 i18next.use(initReactI18next).init({
   // if you're using a language detector, do not define the lng option
