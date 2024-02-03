182d793f16eabb6d1d0350ad84c82704652c8eca
iower <iower@tutanota.com>
2024-02-03 14:54:52 +0500

    Fix DemoMode

--- src/kit/DemoMode.tsx
+++ src/kit/DemoMode.tsx
@@ -1,12 +1,11 @@
+import { useInitData } from '@vkruglikov/react-telegram-web-app'
 import { useTranslation } from 'react-i18next'
 
-import { useStore } from '../store'
-
 const DemoMode = () => {
-  const { txId } = useStore()
+  const [, initData] = useInitData()
   const { t } = useTranslation()
 
-  return txId !== null ? null : (
+  return !!initData ? null : (
     <div className="fixed top-0 width-auto left-[50%] -translate-x-[50%] px-4 py-[1px] text-[13px] leading-[1em] font-semibold bg-[#3a3] text-white rounded-b-md">{t('demoMode')}</div>
   )
 }
