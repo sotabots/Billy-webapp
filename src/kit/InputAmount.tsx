cdaa4647f946395376414af4cbef9cd7c625fd3f
iower <iower@tutanota.com>
2024-04-29 14:14:10 +0500

    InputAmount: improve dark

--- src/kit/InputAmount.tsx
+++ src/kit/InputAmount.tsx
@@ -86,7 +86,7 @@ function InputAmount({ amount, onChange }: TInputAmount) {
   return (
     <input
       type="text"
-      className="w-[117px] h-10 p-2 rounded-md border border-[#DDE2E4] dark:border-[#6E7C87] dark:bg-[#D5DADD] text-right text-[16px] text-text dark:text-[#48535B] leading-[24px] focus:ring-2 focus:ring-button focus:outline-none appearance-none transition-all selection:bg-button selection:text-buttonText"
+      className="w-[117px] h-10 p-2 rounded-md border border-[#DDE2E4] dark:border-[#6E7C87] dark:bg-[#303940] text-right text-[16px] text-text dark:text-[#FFFFFF] leading-[24px] focus:ring-2 focus:ring-button focus:outline-none appearance-none transition-all selection:bg-button selection:text-buttonText"
       inputMode="decimal"
       placeholder="0"
       value={currentString}
