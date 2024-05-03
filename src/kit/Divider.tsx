8fd1d7062b25686e53b891e83a064824df813abf
iower <iower@tutanota.com>
2024-05-03 19:11:38 +0500

    Settings: microfixes

--- src/kit/Divider.tsx
+++ src/kit/Divider.tsx
@@ -5,7 +5,7 @@ function Divider({ className }: {
 }) {
   return (
     <div className={cx(
-      'Divider h-[1.1px] -my-[0.5px] mx-4 bg-[#E5E9EB] dark:bg-[#84919A]',
+      'Divider h-[1.5px] -my-[0.5px] mx-4 bg-[#E5E9EB] dark:bg-[#84919A]',
       className,
      )}
     />
--- src/kit/MenuItem.tsx
+++ src/kit/MenuItem.tsx
@@ -1,5 +1,7 @@
 import { ReactComponent as SettingsGoIcon } from '../assets/settings-go.svg'
 
+import Button from './Button'
+
 const MenuItem = ({ icon, title, value, onClick }: {
   icon: any
   title: string
@@ -7,19 +9,23 @@ const MenuItem = ({ icon, title, value, onClick }: {
   onClick: VoidFunction
 }) => {
   return (
-    <button
-      className="MenuItem flex items-center justify-between gap-4 w-full min-h-[44px] bg-bg px-4 py-2"
+    <Button
+      theme="clear"
+      className="flex items-center justify-between gap-4 w-full min-h-[44px] bg-bg px-4 py-2"
       onClick={onClick}
-    >
-      <div className="flex items-center gap-2">
-        <span>{icon}</span>
-        <span>{title}</span>
-      </div>
-      <div className="flex items-center gap-1">
-        <span className="opacity-50">{value}</span>
-        <span className="opacity-30"><SettingsGoIcon /></span>
-      </div>
-    </button>
+      text={
+        <>
+          <div className="flex items-center gap-2">
+            <span>{icon}</span>
+            <span>{title}</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <span className="opacity-50">{value}</span>
+            <span className="opacity-30"><SettingsGoIcon /></span>
+          </div>
+        </>
+      }
+    />
   )
 }
 
