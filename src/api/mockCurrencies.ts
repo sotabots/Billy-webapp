39988c906db4b31bb744eef1337bf0bb84f061e2
iower <iower@tutanota.com>
2024-05-07 11:22:28 +0500

    Upd mockCurrencies

--- src/api/mockCurrencies.ts
+++ src/api/mockCurrencies.ts
@@ -2,33 +2,283 @@ import { TCurrency } from '../types'
 
 export const mockCurrencies: TCurrency[] = [
   {
-    _id: 'RUB',
-    symbol: '₽',
-    title: {
-      en: 'Ruble',
-      ru: 'Рубль',
-      uk: 'Рубль',
-    },
-    flag: '🇷🇺',
+      _id: "USD",
+      title: {
+          en: "Dollar",
+          ru: "Доллар",
+          uk: "Доллар"
+      },
+      symbol: "$",
+      flag: "🇺🇸"
   },
   {
-    _id: 'EUR',
-    symbol: '€',
-    title: {
-      en: 'Euro',
-      ru: 'Евро',
-      uk: 'Євро',
-    },
-    flag: '🇪🇺',
+      _id: "VND",
+      title: {
+          en: "Dong",
+          ru: "Донг",
+          uk: "Донг"
+      },
+      symbol: "đ",
+      flag: "🇻🇳"
   },
   {
-    _id: "USD",
-    symbol: "$",
-    title: {
-      en: 'Dollar',
-      ru: 'Доллар',
-      uk: 'Долар',
-    },
-    flag: '🇺🇸',
+      _id: "EUR",
+      title: {
+          en: "Euro",
+          ru: "Евро",
+          uk: "Евро"
+      },
+      symbol: "€",
+      flag: "🇪🇺"
   },
+  {
+      _id: "RUB",
+      title: {
+          en: "Rubles",
+          ru: "Рубли",
+          uk: "Рубли"
+      },
+      symbol: "₽",
+      flag: "🇷🇺"
+  },
+  {
+      _id: "GEL",
+      title: {
+          en: "Lari",
+          ru: "Лари",
+          uk: "Ларi"
+      },
+      symbol: "₾",
+      flag: "🇬🇪"
+  },
+  {
+      _id: "TRY",
+      title: {
+          en: "Lira",
+          ru: "Лира",
+          uk: "Лiра"
+      },
+      symbol: "₺",
+      flag: "🇹🇷"
+  },
+  {
+      _id: "GBP",
+      title: {
+          en: "Pound",
+          ru: "Фунт",
+          uk: "Фунт"
+      },
+      symbol: "£",
+      flag: "🇬🇧"
+  },
+  {
+      _id: "UAH",
+      title: {
+          en: "Hryvna",
+          ru: "Гривна",
+          uk: "Грiвна"
+      },
+      symbol: "₴",
+      flag: "🇺🇦"
+  },
+  {
+      _id: "KZT",
+      title: {
+          en: "Tenge",
+          ru: "Тенге",
+          uk: "Тенге"
+      },
+      symbol: "₸",
+      flag: "🇰🇿"
+  },
+  {
+      _id: "BYN",
+      title: {
+          en: "Ruble",
+          ru: "Рубль",
+          uk: "Рубль"
+      },
+      symbol: "Br",
+      flag: "🇧🇾"
+  },
+  {
+      _id: "AZN",
+      title: {
+          en: "Manat",
+          ru: "Манат",
+          uk: "Манат"
+      },
+      symbol: "₼",
+      flag: "🇦🇿"
+  },
+  {
+      _id: "AMD",
+      title: {
+          en: "Dram",
+          ru: "Драм",
+          uk: "Драм"
+      },
+      symbol: "֏",
+      flag: "🇦🇲"
+  },
+  {
+      _id: "MDL",
+      title: {
+          en: "Leu",
+          ru: "Лев",
+          uk: "Лев"
+      },
+      symbol: "L",
+      flag: "🇲🇩"
+  },
+  {
+      _id: "KGS",
+      title: {
+          en: "Som",
+          ru: "Сом",
+          uk: "Сом"
+      },
+      symbol: "с",
+      flag: "🇰🇬"
+  },
+  {
+      _id: "TJS",
+      title: {
+          en: "Somoni",
+          ru: "Сомони",
+          uk: "Сомони"
+      },
+      symbol: "ЅМ",
+      flag: "🇹🇯"
+  },
+  {
+      _id: "UZS",
+      title: {
+          en: "Som",
+          ru: "Сом",
+          uk: "Сом"
+      },
+      symbol: "сўм",
+      flag: "🇺🇿"
+  },
+  {
+      _id: "KRW",
+      title: {
+          en: "Won",
+          ru: "Вон",
+          uk: "Вон"
+      },
+      symbol: "₩",
+      flag: "🇰🇷"
+  },
+  {
+      _id: "JPY",
+      title: {
+          en: "Yen",
+          ru: "Иена",
+          uk: "Иена"
+      },
+      symbol: "¥",
+      flag: "🇯🇵"
+  },
+  {
+      _id: "CNY",
+      title: {
+          en: "Yuan",
+          ru: "Юань",
+          uk: "Юань"
+      },
+      symbol: "¥",
+      flag: "🇨🇳"
+  },
+  {
+      _id: "RSD",
+      title: {
+          en: "Dinar",
+          ru: "Динар",
+          uk: "Дiнар"
+      },
+      symbol: "дин",
+      flag: "🇷🇸"
+  },
+  {
+      _id: "THB",
+      title: {
+          en: "Baht",
+          ru: "Бат",
+          uk: "Бат"
+      },
+      symbol: "฿",
+      flag: "🇹🇭"
+  },
+  {
+      _id: "ZAR",
+      title: {
+          en: "Rand",
+          ru: "Рэнд",
+          uk: "Рэнд"
+      },
+      symbol: "R",
+      flag: "🇿🇦"
+  },
+  {
+      _id: "AED",
+      title: {
+          en: "Dirham",
+          ru: "Дирхам",
+          uk: "Дiрхам"
+      },
+      symbol: "Dh",
+      flag: "🇦🇪"
+  },
+  {
+      _id: "SCR",
+      title: {
+          en: "Rupee",
+          ru: "Рупии",
+          uk: "Рупии"
+      },
+      symbol: "Rs",
+      flag: "🇸🇨"
+  },
+  {
+      _id: "DZD",
+      title: {
+          en: "Dinar",
+          ru: "Динар",
+          uk: "Дiнар"
+      },
+      symbol: "DA",
+      flag: "🇩🇿"
+  },
+  {
+      _id: "INR",
+      title: {
+          en: "Rupee",
+          ru: "Рупии",
+          uk: "Рупии"
+      },
+      symbol: "₹",
+      flag: "🇮🇳"
+  },
+  {
+      _id: "IDR",
+      title: {
+          en: "Rupee",
+          ru: "Рупии",
+          uk: "Рупии"
+      },
+      symbol: "Rp",
+      flag: "🇮🇩"
+  },
+  {
+      _id: "ILS",
+      title: {
+          en: "Shekel",
+          ru: "Шекели",
+          uk: "Шекелi"
+      },
+      symbol: "₪",
+      flag: "🇮🇱"
+  }
 ]
