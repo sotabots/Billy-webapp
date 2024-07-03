f440287bb0af65f041e93c5551df1f9f794d317a
iower <iower@tutanota.com>
2024-07-03 14:58:45 +0500

    Pager

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -12,6 +12,12 @@ import onboarding1 from '../assets/onboarding-1.webp'
 import onboarding3 from '../assets/onboarding-3.webp'
 import onboarding4 from '../assets/onboarding-4.webp'
 
+const Pager = ({ page }: {
+  page: number
+}) => (
+  <div className="absolute right-4 top-4 --bottom-[14px] --left-[50%] --translate-x-[50%] rounded-full px-3 py-1 bg-white font-bold shadow-md border border-[#eee] text-[#aaa]">{page}/5</div>
+)
+
 function Onboarding() {
   // const { t, i18n } = useTranslation()
   // const { feedback } = useFeedback()
@@ -30,10 +36,13 @@ function Onboarding() {
       {step === 1 && (
         <>
           <div
-            className="h-[38vh] bg-center bg-cover"
+            className="relative h-[38vh] bg-center bg-cover"
             style={{ backgroundImage: `url(${onboarding1})` }}
-          ></div>
-          <div className="flex flex-col gap-3 max-w-[500px] px-4 py-5">
+          >
+            {/* <div className="absolute left-0 bottom-0 w-full h-[50px] bg-gradient-to-b from-transparent to-white" /> */}
+            <Pager page={1} />
+          </div>
+          <div className="flex flex-col gap-3 max-w-[500px] px-4 py-8">
             <h2 className="text-[24px]">Революционная запись трат</h2>
             <p>Просто добавь меня в ваш чат, чтобы начать пользоваться ботом. Траты можно записывать как текстом, так и голосовыми сообщениями! Я найду эти сообщения в чате и всё учту.</p>
           </div>
@@ -43,10 +52,12 @@ function Onboarding() {
       {step === 2 && (
         <>
           <div
-            className="h-[38vh] bg-center bg-cover"
+            className="relative h-[38vh] bg-center bg-cover"
             style={{ backgroundImage: `url(${onboarding1})` }}
-          ></div>
-          <div className="flex flex-col gap-3 max-w-[500px] px-4 py-5">
+          >
+            <Pager page={2} />
+          </div>
+          <div className="flex flex-col gap-3 max-w-[500px] px-4 py-8">
             <h2 className="text-[24px]">Надоело переводить друг другу деньги после каждой общей оплаты?</h2>
             <p>Достаточно, чтобы кто-то один написал о трате в чат – Билли это увидит и запишет в историю. В конце периода он просуммирует все траты и скажет, кто кому сколько должен.</p>
           </div>
@@ -56,10 +67,12 @@ function Onboarding() {
       {step === 3 && (
         <>
           <div
-            className="h-[38vh] bg-center bg-cover"
+            className="relative h-[38vh] bg-center bg-cover"
             style={{ backgroundImage: `url(${onboarding3})` }}
-          ></div>
-          <div className="flex flex-col gap-3 max-w-[500px] px-4 py-5">
+          >
+            <Pager page={3} />
+          </div>
+          <div className="flex flex-col gap-3 max-w-[500px] px-4 py-8">
             <h2 className="text-[24px]">Никто не хочет брать ответственность за оплату общих расходов?</h2>
             <p>Плати за друзей - получай кэшбэк в Билли реальными деньгами!</p>
           </div>
@@ -69,10 +82,12 @@ function Onboarding() {
       {step === 4 && (
         <>
           <div
-            className="h-[38vh] bg-center bg-cover"
+            className="relative h-[38vh] bg-center bg-cover"
             style={{ backgroundImage: `url(${onboarding4})` }}
-          ></div>
-          <div className="flex flex-col gap-3 max-w-[500px] px-4 py-5">
+          >
+            <Pager page={4} />
+          </div>
+          <div className="flex flex-col gap-3 max-w-[500px] px-4 py-8">
             <h2 className="text-[24px]">Не встанем из-за стола, пока не посчитаем, кто сколько съел?</h2>
             <p>Запиши трату сейчас, а друзья заполнят свои доли потом.</p>
           </div>
@@ -82,10 +97,12 @@ function Onboarding() {
       {step === 5 && (
         <>
           <div
-            className="h-[38vh] bg-center bg-cover"
+            className="relative h-[38vh] bg-center bg-cover"
             style={{ backgroundImage: `url(${onboarding4})` }}
-          ></div>
-          <div className="flex flex-col gap-4 max-w-[500px] px-4 py-5">
+          >
+            <Pager page={5} />
+          </div>
+          <div className="flex flex-col gap-4 max-w-[500px] px-4 py-8">
             <h2 className="text-[24px]">Самое удобное ведение группового бюджета!</h2>
 
             <div className="flex items-start gap-3">
