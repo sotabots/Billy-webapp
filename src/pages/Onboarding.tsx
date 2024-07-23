e3449c49fa0c1ae2c3ec569de0c8c1be921df85c
iower <iower@tutanota.com>
2024-07-23 13:40:52 +0500

    Onboarding: swipes

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -16,6 +16,8 @@ import onboarding3 from '../assets/onboarding-3.jpg'
 import onboarding4 from '../assets/onboarding-4.jpg'
 import onboarding5 from '../assets/onboarding-5.jpg'
 
+import { useSwipeable } from 'react-swipeable'
+
 const Pager = ({ page }: {
   page: number
 }) => (
@@ -31,6 +33,15 @@ function Onboarding() {
   const [step, setStep] = useState(1)
   const [isButtonBusy, setIsButtonBusy] = useState(false)
 
+  const swipes = useSwipeable({
+    onSwipedLeft: () => {
+      setStep(Math.min(5, step + 1))
+    },
+    onSwipedRight: () => {
+      setStep(Math.max(1, step - 1))
+    }
+  })
+
   return (
     <Screen className="">
       <Header onBack={() => {
@@ -41,107 +52,109 @@ function Onboarding() {
         }
       }} />
 
-      {step === 1 && (
-        <>
-          <div
-            className="relative h-[37vh] bg-[#ffca6a] bg-center bg-cover bg-no-repeat"
-            style={{ backgroundImage: `url(${onboarding1})` }}
-          >
-            <Pager page={1} />
-          </div>
-          <div className="flex flex-col gap-5 max-w-[500px] mx-auto px-4 py-6">
-            <h2 className="text-[24px]">Революционная запись трат</h2>
-            <p>Просто добавь меня в ваш чат, чтобы начать пользоваться ботом. Траты можно записывать как текстом, так и голосовыми сообщениями! Я найду эти сообщения в чате и всё учту.</p>
-          </div>
-        </>
-      )}
-
-      {step === 2 && (
-        <>
-          <div
-            className="relative h-[37vh] bg-[#ffca6a] bg-center bg-cover bg-no-repeat"
-            style={{ backgroundImage: `url(${onboarding2})` }}
-          >
-            <Pager page={2} />
-          </div>
-          <div className="flex flex-col gap-5 max-w-[500px] mx-auto px-4 py-6">
-            <h2 className="text-[24px]">Надоело переводить друг другу деньги после каждой общей оплаты?</h2>
-            <p>Достаточно, чтобы кто-то один написал о трате в чат – Билли это увидит и запишет в историю. В конце периода он просуммирует все траты и скажет, кто кому сколько должен.</p>
-          </div>
-        </>
-      )}
-
-      {step === 3 && (
-        <>
-          <div
-            className="relative h-[37vh] bg-[#ffca6a] bg-center bg-cover bg-no-repeat"
-            style={{ backgroundImage: `url(${onboarding3})` }}
-          >
-            <Pager page={3} />
-          </div>
-          <div className="flex flex-col gap-5 max-w-[500px] mx-auto px-4 py-6">
-            <h2 className="text-[24px]">Никто не хочет брать ответственность за оплату общих расходов?</h2>
-            <p>Плати за друзей - получай кэшбэк в Билли реальными деньгами!</p>
-          </div>
-        </>
-      )}
-
-      {step === 4 && (
-        <>
-          <div
-            className="relative h-[37vh] bg-[#ffca6a] bg-center bg-cover bg-no-repeat"
-            style={{ backgroundImage: `url(${onboarding4})` }}
-          >
-            <Pager page={4} />
-          </div>
-          <div className="flex flex-col gap-5 max-w-[500px] mx-auto px-4 py-6">
-            <h2 className="text-[24px]">Не встанем из-за стола, пока не посчитаем, кто сколько съел?</h2>
-            <p>Запиши трату сейчас, а друзья заполнят свои доли потом.</p>
-          </div>
-        </>
-      )}
-
-      {step === 5 && (
-        <>
-          <div
-            className="relative h-[37vh] bg-[#ffca6a] bg-center bg-cover bg-no-repeat"
-            style={{ backgroundImage: `url(${onboarding5})` }}
-          >
-            <Pager page={5} />
-          </div>
-          <div className="flex flex-col gap-5 max-w-[500px] mx-auto px-4 py-6">
-            <h2 className="text-[24px]">Самое удобное ведение группового бюджета!</h2>
-
-            <div className="flex flex-col gap-3">
-              <div className="flex items-start gap-3">
-                <CheckmarkIcon className="flex-shrink-0 w-[22px] h-[22px] text-button" />
-                <div className="">
-                  <h3>Конвертация валют</h3>
-                  <p className="text-[14px]">Упростит финальные расчёты, пересчитав все траты в выбранную валюту. Особенно актуально в путешествиях!</p>
-                </div>
-              </div>
-
-              <div className="flex items-start gap-3">
-                <CheckmarkIcon className="flex-shrink-0 w-[22px] h-[22px] text-button" />
-                <div className="">
-                  <h3>Категории</h3>
-                  <p className="text-[14px]">Билли определит категорию для каждой траты, а в конце покажет анализ самых больших из них</p>
-                </div>
-              </div>
-
-              {/*
-              <div className="flex items-start gap-3">
-                <CheckmarkIcon className="flex-shrink-0 w-[22px] h-[22px] text-button" />
-                <div className="">
-                  <h3>Настройки языка и валюты</h3>
-                  <p className="text-[14px]">Выбери удобный язык и валюту для ведения общих трат</p>
-                </div>
-              </div>
-              */}
+      <div className="Swipes" {...swipes}>
+        {step === 1 && (
+          <>
+            <div
+              className="relative h-[37vh] bg-[#ffca6a] bg-center bg-cover bg-no-repeat"
+              style={{ backgroundImage: `url(${onboarding1})` }}
+            >
+              <Pager page={1} />
             </div>
-          </div>
-        </>
-      )}
+            <div className="flex flex-col gap-5 max-w-[500px] mx-auto px-4 py-6">
+              <h2 className="text-[24px]">Революционная запись трат</h2>
+              <p>Просто добавь меня в ваш чат, чтобы начать пользоваться ботом. Траты можно записывать как текстом, так и голосовыми сообщениями! Я найду эти сообщения в чате и всё учту.</p>
+            </div>
+          </>
+        )}
+
+        {step === 2 && (
+          <>
+            <div
+              className="relative h-[37vh] bg-[#ffca6a] bg-center bg-cover bg-no-repeat"
+              style={{ backgroundImage: `url(${onboarding2})` }}
+            >
+              <Pager page={2} />
+            </div>
+            <div className="flex flex-col gap-5 max-w-[500px] mx-auto px-4 py-6">
+              <h2 className="text-[24px]">Надоело переводить друг другу деньги после каждой общей оплаты?</h2>
+              <p>Достаточно, чтобы кто-то один написал о трате в чат – Билли это увидит и запишет в историю. В конце периода он просуммирует все траты и скажет, кто кому сколько должен.</p>
+            </div>
+          </>
+        )}
+
+        {step === 3 && (
+          <>
+            <div
+              className="relative h-[37vh] bg-[#ffca6a] bg-center bg-cover bg-no-repeat"
+              style={{ backgroundImage: `url(${onboarding3})` }}
+            >
+              <Pager page={3} />
+            </div>
+            <div className="flex flex-col gap-5 max-w-[500px] mx-auto px-4 py-6">
+              <h2 className="text-[24px]">Никто не хочет брать ответственность за оплату общих расходов?</h2>
+              <p>Плати за друзей - получай кэшбэк в Билли реальными деньгами!</p>
+            </div>
+          </>
+        )}
+
+        {step === 4 && (
+          <>
+            <div
+              className="relative h-[37vh] bg-[#ffca6a] bg-center bg-cover bg-no-repeat"
+              style={{ backgroundImage: `url(${onboarding4})` }}
+            >
+              <Pager page={4} />
+            </div>
+            <div className="flex flex-col gap-5 max-w-[500px] mx-auto px-4 py-6">
+              <h2 className="text-[24px]">Не встанем из-за стола, пока не посчитаем, кто сколько съел?</h2>
+              <p>Запиши трату сейчас, а друзья заполнят свои доли потом.</p>
+            </div>
+          </>
+        )}
+
+        {step === 5 && (
+          <>
+            <div
+              className="relative h-[37vh] bg-[#ffca6a] bg-center bg-cover bg-no-repeat"
+              style={{ backgroundImage: `url(${onboarding5})` }}
+            >
+              <Pager page={5} />
+            </div>
+            <div className="flex flex-col gap-5 max-w-[500px] mx-auto px-4 py-6">
+              <h2 className="text-[24px]">Самое удобное ведение группового бюджета!</h2>
+
+              <div className="flex flex-col gap-3">
+                <div className="flex items-start gap-3">
+                  <CheckmarkIcon className="flex-shrink-0 w-[22px] h-[22px] text-button" />
+                  <div className="">
+                    <h3>Конвертация валют</h3>
+                    <p className="text-[14px]">Упростит финальные расчёты, пересчитав все траты в выбранную валюту. Особенно актуально в путешествиях!</p>
+                  </div>
+                </div>
+
+                <div className="flex items-start gap-3">
+                  <CheckmarkIcon className="flex-shrink-0 w-[22px] h-[22px] text-button" />
+                  <div className="">
+                    <h3>Категории</h3>
+                    <p className="text-[14px]">Билли определит категорию для каждой траты, а в конце покажет анализ самых больших из них</p>
+                  </div>
+                </div>
+
+                {/*
+                <div className="flex items-start gap-3">
+                  <CheckmarkIcon className="flex-shrink-0 w-[22px] h-[22px] text-button" />
+                  <div className="">
+                    <h3>Настройки языка и валюты</h3>
+                    <p className="text-[14px]">Выбери удобный язык и валюту для ведения общих трат</p>
+                  </div>
+                </div>
+                */}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
 
       <Button
         isBottom
