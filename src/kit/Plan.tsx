86c9e52d7341505e1086f9222ae71070c7ca3b38
iower <iower@tutanota.com>
2024-08-17 15:14:45 +0500

    Plans

---
+++ src/kit/Plan.tsx
@@ -0,0 +1,79 @@
+import cx from 'classnames'
+
+import { useTranslation } from 'react-i18next'
+
+import Button from './Button'
+
+import star from '../assets/star.png'
+
+const textGradient = {
+  background: 'linear-gradient(85.8deg, #1C6ED8 3.42%, rgba(12, 215, 228, 0.99) 96.58%)',
+  '-webkit-background-clip': 'text',
+  'background-clip': 'text',
+  '-webkit-text-fill-color': 'transparent',
+}
+
+function Plan({ className, label, labelColor, title, stars, fiat, discount, isActive, onClick }: {
+  className?: string
+  label?: string
+  labelColor?: string
+  title: string
+  stars: number
+  fiat: string
+  discount?: string
+  isActive: boolean
+  onClick: (_: number) => void
+}) {
+  const { t } = useTranslation()
+
+  return (
+    <Button
+      theme="clear"
+      className={cx(
+        'Plan w-full',
+        className,
+      )}
+      onClick={() => { onClick(stars) }}
+      text={
+        <div className={cx(
+          'relative rounded-[16px] p-[2px] transition-all',
+          isActive ? 'bg-gradient-to-r from-[#1C6ED8] to-[#0CD7E4FC]' : 'bg-[#DDE2E4]',
+          label && 'mt-[14px]'
+        )}>
+          <div className="rounded-[14px] bg-bg text-[#6E7C87] text-left">
+            {label &&
+              <div
+                className="absolute left-[10px] -top-[14px] inline-block mb-1 px-2 py-[2px] rounded-[20px] text-[#ffffff] text-[13px] leading-[18px] font-semibold"
+                style={{ backgroundColor: labelColor }}
+              >
+                {label}
+              </div>
+            }
+            <div className="flex items-start gap-2 px-3 py-2">
+              <div className="w-full flex flex-col gap-[2px]">
+                <div className={cx(
+                  'font-semibold text-[16px] leading-[24px] transition-all',
+                  isActive ? 'text-text' : 'text-[#5B6871]'
+                )}>{title}</div>
+                <div className="flex items-center gap-1 text-[16px] leading-[24px]">
+                  <img src={star} className="w-4 h-4" />
+                  <span className="">{stars} {t('stars')}</span>
+                  <span className="">•</span>
+                  <span className="">{fiat}</span>
+                </div>
+              </div>
+              {discount &&
+                <div className="" style={{ ...textGradient }}>
+                  <div className="text-[12px] leading-[16px] font-semibold">{t('discount')}</div>
+                  <div className="text-[24px] leading-[32px] font-semibold">{discount}</div>
+                </div>
+              }
+            </div>
+          </div>
+        </div>
+      }
+    />
+  )
+}
+
+export default Plan
--- src/pages/Paywall.tsx
+++ src/pages/Paywall.tsx
@@ -1,11 +1,13 @@
 import Lottie from 'lottie-react'
-import { /* useEffect, useState */ } from 'react'
+import { /* useEffect,*/ useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import Button from '../kit/Button'
 import Header from '../kit/Header'
 import Screen from '../kit/Screen'
 import Panel from '../kit/Panel'
+import Divider from '../kit/Divider'
+import Plan from '../kit/Plan'
 
 import { useInit /*, useFeedback */ } from '../hooks'
 
@@ -39,6 +41,8 @@ function Paywall() {
 
   const link = 'https://t.me/$57jAnvUn6ElOIwMAscGfUwoljzY'
 
+  const [plan, setPlan] = useState(20)
+
   return (
     <Screen>
       <Header onBack={() => { history.back() }} />
@@ -68,18 +72,61 @@ function Paywall() {
         </div>
       </Panel>
 
-      <Button
-        theme="clear"
-        wrapperClassName="mt-10"
-        className="flex items-center justify-center gap-2 w-full h-[50px] rounded-[6px] bg-gradient-to-r from-[#1C6ED8] to-[#0CD7E4] text-[#F6F8F9] text-[14px] font-semibold"
-        text={
-          <>
-            <span>{t('buyFor')} 80</span>
-            <img src={star} className="w-6 h-6" />
-          </>
-        }
-        onClick={() => { location.replace(link) }}
-      />
+      <div className="p-4 pb-6">
+        <Plan
+          label={t('forWeekend')}
+          labelColor={'#000000'}
+          title={`3 ${t('days')}`}
+          stars={20}
+          fiat={`299₽`}
+          isActive={plan === 20}
+          onClick={() => { setPlan(20) }}
+        />
+
+        <Divider className="my-3 mx-0" />
+
+        <div className="flex flex-col gap-2">
+          <Plan
+            label={t('profitable')}
+            labelColor={'#4094F7'}
+            title={`12 ${t('months')}`}
+            stars={2500}
+            fiat={`4999₽`}
+            discount={'50%'}
+            isActive={plan === 2500}
+            onClick={() => { setPlan(2500) }}
+          />
+          <Plan
+            title={`1 ${t('month')}`}
+            stars={250}
+            fiat={`1999₽`}
+            discount={'30%'}
+            isActive={plan === 250}
+            onClick={() => { setPlan(250) }}
+          />
+          <Plan
+            title={`7 ${t('days')}`}
+            stars={80}
+            fiat={`699₽`}
+            isActive={plan === 80}
+            onClick={() => { setPlan(80) }}
+          />
+        </div>
+      </div>
+      <div className="sticky px-4 py-2 bottom-0">
+        <Button
+          theme="clear"
+          wrapperClassName="mt-5"
+          className="flex items-center justify-center gap-2 w-full h-[50px] rounded-[6px] bg-gradient-to-r from-[#1C6ED8] to-[#0CD7E4] text-[#F6F8F9] text-[14px] font-semibold"
+          text={
+            <>
+              <span>{t('buyFor')} {plan}</span>
+              <img src={star} className="w-6 h-6" />
+            </>
+          }
+          onClick={() => { location.replace(link) }}
+        />
+      </div>
     </Screen>
   )
 }
