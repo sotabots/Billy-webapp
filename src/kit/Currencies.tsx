7951d4a6d046f20c854fc2c70776b7dd1485196d
iower <iower@tutanota.com>
2024-08-28 14:30:45 +0500

    Move out Currencies

---
+++ src/kit/Currencies.tsx
@@ -0,0 +1,44 @@
+import cx from 'classnames'
+import { useTranslation } from 'react-i18next'
+
+import Divider from '../kit/Divider'
+import RadioButton from '../kit/RadioButton'
+
+import { TCurrencyId, TLanguageCode } from '../types'
+import { useStore } from '../hooks'
+
+function Currencies({ className, value, onChange }: {
+  className?: string
+  value?: TCurrencyId | null
+  onChange: (currencyId: TCurrencyId) => void
+}) {
+  const { i18n } = useTranslation()
+  const { currencies } = useStore()
+  return (
+    <div className={cx('Currencies overflow-y-auto', className)}>
+      {currencies.map((currency, i) => (
+        <div key={`currencies-${currency._id}`}>
+          <RadioButton
+            group="currencies"
+            label={(
+              <>
+                <span className="font-semibold">{currency.symbol}</span>
+                {' '}
+                <span>{currency.flag}</span>
+                {' '}
+                <span>{currency.title[i18n.language as TLanguageCode]}</span>
+              </>
+            )}
+            key={`currencies-${currency._id}`}
+            value={currency._id}
+            checked={value === currency._id}
+            onChange={onChange}
+          />
+          {i < currencies.length - 1 && <Divider key={`Divider-${i}`} />}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Currencies
--- src/pages/SelectCurrency.tsx
+++ src/pages/SelectCurrency.tsx
@@ -3,20 +3,19 @@ import { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 
-import Divider from '../kit/Divider'
 import Header from '../kit/Header'
-import RadioButton from '../kit/RadioButton'
 import Screen from '../kit/Screen'
+import Currencies from '../kit/Currencies'
 
 import { useStore, useFeedback, useInit } from '../hooks'
-import { TCurrencyId, TLanguageCode } from '../types'
+import { TCurrencyId } from '../types'
 
 function SelectCurrency() {
   useInit()
 
-  const { t, i18n } = useTranslation()
+  const { t } = useTranslation()
   const navigate = useNavigate()
-  const { currencies, transaction, setCurrency } = useStore()
+  const { transaction, setCurrency } = useStore()
   const [impactOccurred, , selectionChanged] = useHapticFeedback()
   const { feedback } = useFeedback()
 
@@ -40,33 +39,11 @@ function SelectCurrency() {
       <div className="px-4">
         <h2>{t('selectCurrency')}</h2>
       </div>
-      <div className="mt-4 overflow-y-auto">
-        {currencies.map((currency, i) => (
-          <div key={`currencies-${currency._id}`}>
-            <RadioButton
-              reverse
-              group="currencies"
-              label={(
-                <div className="flex gap-2">
-                  <div className="w-8 h-8 bg-text/5 rounded-full overflow-hidden">
-                    <span className="text-8 leading-8">{currency.flag}</span>
-                  </div>
-                  <div className="flex flex-col -gap-[2px]">
-                    {/* <span className="font-semibold">{currency.symbol}</span> */}
-                    <div className="text-[14px] leading-[20px]">{currency.title[i18n.language as TLanguageCode]}</div>
-                    <div className="text-[12px] leading-[16px] uppercase text-hint">{currency._id}</div>
-                  </div>
-                </div>
-              )}
-              key={`currencies-${currency._id}`}
-              value={currency._id}
-              checked={transaction?.currency_id === currency._id}
-              onChange={onChange}
-            />
-            {i < currencies.length - 1 && <Divider key={`Divider-${i}`} />}
-          </div>
-        ))}
-      </div>
+      <Currencies
+        className="mt-4"
+        value={transaction?.currency_id}
+        onChange={onChange}
+      />
     </Screen>
   )
 }
